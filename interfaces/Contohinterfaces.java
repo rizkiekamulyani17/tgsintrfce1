06aa8569748d6c6074bb05a1e03b016f507f0b03
Your Name <you@example.com>
2022-04-27 19:07:51 +0700

    Initial commit

---
+++ interfaces/Contohinterfaces.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface Contohinterfaces {
+    public void PrintJudul();
+    public void HitungTambah();
+    public void Hitungkali();
+
+    void HitungKali();
+}
